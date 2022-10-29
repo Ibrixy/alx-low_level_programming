92331ae1fd30d91985d7cb68334a9533355581c4
ibrixy <ibrahimsquared14@gmail.com>
2022-10-29 11:35:35 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,12 +7,18 @@
  */
 int print_last_digit(int n)
 {
-	int ld = n % 10;
+	int r;
 
-	if (ld < 0)
-		ld += -1;
-
-	_putchar(ld + '0');
-
-	return (0);
+	if (n < 0)
+	{
+		r = -1 * (n % 10);
+		_putchar(r + '0');
+		return (r);
+	}
+	else
+	{
+		r = n % 10;
+		_putchar(r + '0');
+		return (r);
+	}
 }
