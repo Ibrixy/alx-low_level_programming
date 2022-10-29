d4ad229e5676212f5d99be8210666347ac576a37
ibrixy <ibrahimsquared14@gmail.com>
2022-10-29 09:32:15 -0700

    ten times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,7 +3,7 @@
 /**
  * print_alphabet_x10 - print alpha 10 times
  */
-void print_alphabet_x10(void)
+void print-alphabet_x10(void)
 {
 	int ten;
 	char la;
@@ -12,6 +12,8 @@ void print_alphabet_x10(void)
 	{
 	for (la = 'a'; la <= 'z'; la++)
 		_putchar(la);
-	_putchar('\n'');
+	_putchar('\n');
 	}
 }
+
+
